18e85d16be36de8b52fef6454ae0d7439942c9df
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-06-22 11:21:32 -0600

    Added GET /api/ideas/:ideaId route

--- server/ideas.js
+++ server/ideas.js
@@ -8,11 +8,28 @@ const {
     deleteFromDatabasebyId // (model, id) --> true | false
 } = require('./db'); 
 
+// Extract ideaId parameter and attach it to request object
+ideasRouter.param('ideaId', (req, res, next, id) => {
+    req.id = id;
+    next();
+});
+
 // Get all ideas
 ideasRouter.get('/', (req, res, next) => {
     const ideas = getAllFromDatabase('ideas');
 
     res.send(ideas);
+});
+
+// Get a single idea
+ideasRouter.get('/:ideaId', (req, res, next) => {
+    const idea = getFromDatabaseById('ideas', req.id);
+
+    if (idea) {
+        res.send(idea);
+    } else {
+        res.status(404).send();
+    }    
 });
 
 module.exports = ideasRouter;
